f07e90f158d504cf6a692c959b9f0521a2e8862a
Lukas <36337818+lukas-tr@users.noreply.github.com>
2025-09-02 10:50:54 +0200

    basic earth

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,49 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Canvas, useFrame } from '@react-three/fiber'
+import EarthGlobe from './EarthGlobe'
+
+// function Box(props: any) {
+//   // This reference will give us direct access to the mesh
+//   const meshRef = useRef()
+//   // Set up state for the hovered and active state
+//   const [hovered, setHover] = useState(false)
+//   const [active, setActive] = useState(false)
+//   // Subscribe this component to the render-loop, rotate the mesh every frame
+//   useFrame((state, delta) => (meshRef.current.rotation.x += delta))
+//   // Return view, these are regular three.js elements expressed in JSX
+//   return (
+//     <mesh
+//       {...props}
+//       ref={meshRef}
+//       scale={active ? 1.5 : 1}
+//       onClick={(event) => setActive(!active)}
+//       onPointerOver={(event) => setHover(true)}
+//       onPointerOut={(event) => setHover(false)}>
+//       <boxGeometry args={[1, 1, 1]} />
+//       <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+//     </mesh>
+//   )
+// }
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>PAIN</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+    <EarthGlobe />
+         {/* <Canvas>
+    <ambientLight intensity={Math.PI / 2} />
+    <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+    <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+    <Box position={[-1.2, 0, 0]} />
+    <Box position={[1.2, 0, 0]} />
+  </Canvas> */}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ src/EarthGlobe.tsx
@@ -0,0 +1,110 @@
+import { Canvas, useFrame, useLoader, useThree } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import * as THREE from "three";
+import React, { useEffect, useRef } from "react";
+
+// texture source: https://www.solarsystemscope.com/textures/
+
+function EquirectBackground({ src }: { src: string }) {
+    const tex = useLoader(THREE.TextureLoader, src);
+    const { scene } = useThree();
+
+    useEffect(() => {
+        tex.colorSpace = THREE.SRGBColorSpace;
+        tex.mapping = THREE.EquirectangularReflectionMapping;
+        const prev = scene.background;
+        scene.background = tex;
+        return () => { scene.background = prev; };
+    }, [scene, tex]);
+
+    return null;
+}
+
+
+function Earth() {
+    const earthRef = useRef<THREE.Mesh>(null!);
+    const cloudsRef = useRef<THREE.Mesh>(null!);
+    const day = useLoader(THREE.TextureLoader, "/textures/planets/8k_earth_daymap.jpg");
+    const night = useLoader(THREE.TextureLoader, "/textures/planets/earth_night_4096.jpg");
+    const clouds = useLoader(THREE.TextureLoader, "/textures/planets/8k_earth_clouds.jpg");
+    const height = useLoader(THREE.TextureLoader, "/textures/planets/8k_height.jpg");
+
+    useEffect(() => {
+        [day, night, clouds].forEach(t => {
+            t.colorSpace = THREE.SRGBColorSpace;
+            t.anisotropy = 8;
+        });
+
+        [height].forEach(t => {
+            t.colorSpace = THREE.LinearSRGBColorSpace;
+            t.anisotropy = 8;
+            t.wrapS = t.wrapT = THREE.RepeatWrapping;
+        });
+    }, [day, night, clouds, height]);
+
+
+    useFrame((_, delta) => {
+        earthRef.current.rotation.y += delta * 0.025;
+        cloudsRef.current.rotation.y += delta * 0.05;
+    });
+
+    return (
+        <group>
+            {/* Globe */}
+            <mesh ref={earthRef}>
+                <sphereGeometry args={[1, 512, 512]} />
+                <meshStandardMaterial map={day}
+                    displacementMap={height}
+                    displacementScale={-0.2}
+                    displacementBias={0}
+                />
+            </mesh>
+
+            {/* Night lights overlay (additive blend) */}
+            {/* <mesh>
+        <sphereGeometry args={[1.001, 64, 64]} />
+        <meshBasicMaterial map={night} blending={THREE.AdditiveBlending} transparent />
+      </mesh> */}
+
+            {/* Clouds */}
+            <mesh ref={cloudsRef}>
+                <sphereGeometry args={[1.01, 64, 64]} />
+                <meshStandardMaterial
+                    map={clouds}
+                    transparent
+                    opacity={0.1}
+                    depthWrite={false}
+                />
+            </mesh>
+
+            {/* Atmosphere glow */}
+            <mesh>
+                <sphereGeometry args={[1.03, 64, 64]} />
+                <meshBasicMaterial
+                    color="#4db2ff"
+                    transparent
+                    opacity={0.1}
+                    side={THREE.BackSide}
+                    blending={THREE.AdditiveBlending}
+                />
+            </mesh>
+        </group>
+    );
+}
+
+export default function EarthScene() {
+    const key = useRef(0);
+    useEffect(() => {
+        key.current = Math.random(); // needed to force remount during hmr
+    }, [])
+    return (
+        <Canvas style={{ width: "90vw", height: "90vh" }} camera={{ fov: 25, position: [4.5, 2, 3] }}>
+            <EquirectBackground src="/textures/planets/8k_stars_milky_way.jpg" />
+            {/* <color attach="background" args={["#000"]} /> */}
+            <ambientLight intensity={2} />
+            <directionalLight color="#ffdc6aff" intensity={5} position={[0, 0, 3]} />
+            <OrbitControls enableDamping minDistance={0.1} maxDistance={50} />
+            <Earth />
+        </Canvas>
+    );
+}
