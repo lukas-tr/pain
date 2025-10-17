8b11e8cdccedfbb99fcb60c67f1eb8814d6c1e2f
Hollis Hui <svttheone@gmail.com>
2025-10-17 12:05:15 +0000

    text edit

--- src/App.tsx
+++ src/App.tsx
@@ -65,8 +65,7 @@ function App() {
       </div>
         <div className="relative overflow-hidden">
           <h1 className="text-[1.8rem] m-2" style={{textAlign: "center"}}>  ⁠<strong>P.A.I.N</strong> <b>is PERSONAL AND INTERCONNECTED with NATURE</b></h1>
-          <h2 className="text-[1.8rem] m-2"><strong>EXPLORE PERSONAL-PLANETARY-PAIN (PPP)</strong></h2>
-          <p className="m-2"><b>(click anywhere on the map)</b></p>
+          <h2 className="text-[1.8rem] m-2" style={{textAlign: "center"}}><strong>EXPLORE THE PERSONAL-PLANETARY-PAIN (PPP) MAP</strong></h2>
           <EarthGlobe highlightCoords={coords} />
          <Legend />
         </div>
