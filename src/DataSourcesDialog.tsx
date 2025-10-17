daebb3c0bfcecb76e0b531150dbf245e86f2cb72
Hollis Hui <svttheone@gmail.com>
2025-10-17 23:17:00 +0000

    s

--- src/DataSourcesDialog.tsx
+++ src/DataSourcesDialog.tsx
@@ -29,10 +29,11 @@ export function DataSourcesDialog() {
             <div className="flex-grow overflow-scroll">
               <p className="text-[1.6rem] mb-8">
              <img loading="lazy" src="howitworks.jpg" className="max-w-150 m-auto max-w-[100%]"/>
+              <h2 className="text-[2rem] font-[800] mb-4" >Instructions for Using the Map:</h2>
              Navigate the PPP Map by rotating and zooming in on the Earth’s surface. As you travel from one place to another, each point emits a sound associated with one of the five elements: wood, fire, earth, metal, and water. These elements represent the type of environmental pain that the planet is experiencing. 
-              <br></br>
+              <br></br><br></br>
               Use the “Map Legend”to view how the Earth’s pain correlates with Physical Pain (red), Emotional Pain (blue), and Socioeconomic Pain (yellow).
-               <br></br>
+               <br></br><br></br>
               Then click on “Share Your Pain” to share your experience, which will be matched to a location on the planet’s body. This map encourages reflection on your relationship with planetary pain. Enjoy your exploration!
              </p>
             
