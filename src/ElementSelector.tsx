12c53bcc5f7bf760edd857c5a362aa4378493a58
Hollis Hui <svttheone@gmail.com>
2025-10-17 15:30:21 +0000

    j

--- src/ElementSelector.tsx
+++ src/ElementSelector.tsx
@@ -3,7 +3,7 @@
 export default function ElementSelector() {
   return (
     <div className="w-full h-64 bg-gray-200 flex items-center justify-center">
-      <img loading="lazy" src="Elements.png" className="max-w-150 m-auto max-w-[100%]"/>
+      <img loading="lazy" src="Elements.png" className="max-w-150 m-auto max-w-[80%]"/>
 
     </div>
   );
