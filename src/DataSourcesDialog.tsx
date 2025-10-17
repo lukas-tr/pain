e31fd257cb1acb09471238b43e858b849a2d5afd
Hollis Hui <svttheone@gmail.com>
2025-10-17 23:12:24 +0000

    text add

--- src/DataSourcesDialog.tsx
+++ src/DataSourcesDialog.tsx
@@ -27,15 +27,18 @@ export function DataSourcesDialog() {
           <>
             <DialogHeader> <DialogTitle className="text-[2.2rem]"><strong>How it works?</strong></DialogTitle> </DialogHeader>
             <div className="flex-grow overflow-scroll">
-              <img loading="lazy" src="howitworks.jpg" className="max-w-150 m-auto max-w-[100%]"/>
-                Navigate the PPP Map by rotating and zooming in on the Earth’s surface. As you travel from one place to another, each point emits a sound associated with one of the five elements: wood, fire, earth, metal, and water. These elements represent the type of environmental pain that the planet is experiencing. 
+              <p className="text-[1.6rem] mb-8">
+             <img loading="lazy" src="howitworks.jpg" className="max-w-150 m-auto max-w-[100%]"/>
+             Navigate the PPP Map by rotating and zooming in on the Earth’s surface. As you travel from one place to another, each point emits a sound associated with one of the five elements: wood, fire, earth, metal, and water. These elements represent the type of environmental pain that the planet is experiencing. 
               <br></br>
               Use the “Map Legend”to view how the Earth’s pain correlates with Physical Pain (red), Emotional Pain (blue), and Socioeconomic Pain (yellow).
                <br></br>
-              Then click on “Share Your Pain” to share your experience, which will be matched to a location on the planet’s body. This map encourages reflection on your relationship with planetary pain. Enjoy your exploration!</p>
-
+              Then click on “Share Your Pain” to share your experience, which will be matched to a location on the planet’s body. This map encourages reflection on your relationship with planetary pain. Enjoy your exploration!
+             </p>
+            
             </div>
           </>
+
           <DialogFooter className="flex justify-between">
             <DialogClose asChild>
               <button >close</button>
